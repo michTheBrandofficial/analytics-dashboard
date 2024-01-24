bc6df169699ed899bc10aeb047b6080f4b0046ed
michTheBrandofficial <michthebrand@gmail.com>
2024-01-23 22:33:16 -0800

    [completed] Sidebar completed

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { VStack } from "nixix/view-components";
-import {SideBarIcons, ThemeIcons, ToolIcons} from './Icons'
+import { SideBarIcons, ThemeIcons, ToolIcons } from "./Icons";
+import { For } from "nixix/hoc";
 
 const Sidebar = (): someView => {
   return (
@@ -12,24 +13,37 @@ const Sidebar = (): someView => {
       </div>
 
       {/* Sidebar icons */}
-      <div className="w-full h-fit column items-center justify-center bg-transparent gap-4">
-        {SideBarIcons.map(Icon => (
-          <div className="w-fit h-fit cursor-pointer"><Icon /></div>
-        ))}
+      <div className='w-full h-fit column items-center justify-center bg-transparent gap-4 relative'>
+        <For each={SideBarIcons}>
+          {(Icon) => (
+            <div className='w-fit h-fit cursor-pointer'>
+              <Icon />
+            </div>
+          )}
+        </For>
+        <div className="w-1 h-6 rounded-s-full bg-black absolute top-2 right-0" />
       </div>
 
       {/* Theme */}
-      <div className="w-full h-fit column items-center justify-center bg-transparent gap-4">
-        {ThemeIcons.map(Icon => (
-          <div className="w-fit h-fit cursor-pointer"><Icon /></div>
-        ))}
+      <div className='w-full h-fit column items-center justify-center bg-transparent gap-4'>
+        <For each={ThemeIcons}>
+          {(Icon) => (
+            <div className='w-fit h-fit cursor-pointer'>
+              <Icon />
+            </div>
+          )}
+        </For>
       </div>
 
       {/* Tools */}
-      <div className="mt-auto w-full h-fit column items-center justify-center bg-transparent gap-4">
-        {ToolIcons.map(Icon => (
-          <div className="w-fit h-fit cursor-pointer"><Icon /></div>
-        ))}
+      <div className='mt-auto w-full h-fit column items-center justify-center bg-transparent gap-4'>
+        <For each={ToolIcons}>
+          {(Icon) => (
+            <div className='w-fit h-fit cursor-pointer'>
+              <Icon />
+            </div>
+          )}
+        </For>
       </div>
     </VStack>
   );
